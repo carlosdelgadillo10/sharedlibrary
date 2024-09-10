6cbb2cfb3e95c61175c5025479cada34e2ee0002
Carlos Delgadillo <carlos.delgadillo102003@gmail.com>
2024-09-09 23:37:58 -0600

    s

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,4 +1,4 @@
-def pushDockerImage(String dockerHubCredentialsId, String docker_Image, String dockerTag, String dockerHubRepo) {
+def call(String dockerHubCredentialsId, String docker_Image, String dockerTag, String dockerHubRepo) {
     script {
         docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
         docker.image("${docker_image}")push('latest')
