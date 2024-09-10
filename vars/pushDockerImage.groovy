bc31f4f04fc6cb58222eefafe0af463ba1c49267
Carlos Delgadillo <carlos.delgadillo102003@gmail.com>
2024-09-09 23:27:52 -0600

    s

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,8 +1,8 @@
 def call(String dockerHubCredentialsId, String dockerImage, String dockerTag, String dockerHubRepo) {
     script {
-        docker.withRegistry('https://registry.hub.docker.com', dockerHubCredentialsId) {
-            sh "docker tag ${dockerImage}:${dockerTag} ${dockerHubRepo}:${dockerTag}"
-            sh "docker push ${dockerHubRepo}:${dockerTag}"
-        }
+        docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+    docker.image('carlosdelgadillo/sumaa').push('latest')
+}
+
     }
 }
